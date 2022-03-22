14793d03538db71f8286bc5bd79860870ecb9b51
BerkayMehmetSert <berkaymehmetsert@gmail.com>
2022-03-22 20:17:26 +0300

    variables tutorial

---
+++ variables-demo.py
@@ -0,0 +1,33 @@
+'''
+1-) Create variables for the following information:
+    Customer name 
+    Customer last name
+    Customer name + last name
+    Customer gender
+    Customer id
+    CUstomer birth year
+    Customer address
+    Customer age
+'''
+customerName = 'John'
+customerLastname = 'Doe'
+customerNameLastname = customerName + ' ' + customerLastname
+customerGender = 'Male'
+customerId = '22222'
+customerBirthYear = '1990'
+customerAddress = 'New York'
+customerAge = '32'
+
+'''
+
+2-) Calculate the total information of the following orders. 
+    Order 1 => $ 110
+    Order 2 => $ 1100.5
+    Order 3 => $ 356.95
+
+'''
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+total = order1 + order2 + order3
+print('Total: ', total)
